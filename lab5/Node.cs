496a65da4c0f5b58ac011ca9a8100d1925d758c1
burur14 <90176858+burur14@users.noreply.github.com>
2023-01-08 12:16:46 +0200

    Add files via upload

---
+++ lab5/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab5
+{   
+    [Serializable]
+    class Node
+    {
+        public int number;
+        public bool captured;
+        public List<int> neighbours;
+
+        public Node()
+        {
+            number = -1;
+            captured = false;
+            neighbours = new List<int>();
+        }
+
+        public bool connect(Node neighbour, int maxConnections)
+        {
+            if (!this.neighbours.Contains(neighbour.number) && !(neighbour.neighbours.Count == maxConnections))
+            {
+                this.neighbours.Add(neighbour.number);
+                neighbour.neighbours.Add(this.number);
+                return true;
+            }
+            return false;
+        }
+    }
+}
